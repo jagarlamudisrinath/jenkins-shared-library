2307e162c918be8ed20b8788988343329cce6aa2
srinathjagarlamudi <jagarlamudisrinath99@gmail.com>
2024-06-05 21:52:34 -0500

    updated groovy method

--- vars/extract-schedule.groovy
+++ vars/extract-schedule.groovy
@@ -1,7 +1,7 @@
-def call(String name = "World") {
+def call(String yamlFilePath = "World") {
     // Parse the YAML file and convert to JSON
-    def database_name = sh(script: "yq e \'.database\' ${yamlFilePath}", returnStdout: true).trim()
+    def database_name = sh(script: "echo ${yamlFilePath} | yq e \'.database\'", returnStdout: true).trim()
     // Parse the YAML file and convert to JSON
-    def schedule = sh(script: "yq e \'.schdeule\' ${yamlFilePath}", returnStdout: true).trim()
+    def schedule = sh(script: "echo ${yamlFilePath} | yq e \'.schdeule\'", returnStdout: true).trim()
     echo "database name ${database_name}, ${schedule}"
 }
