f21ca12d5f8feff7caf4158bdaed8e59747169bf
srinathjagarlamudi <jagarlamudisrinath99@gmail.com>
2024-06-11 19:58:12 -0500

    Commiting test function

--- vars/extractSchedule.groovy
+++ vars/extractSchedule.groovy
@@ -1,7 +1,8 @@
 def call(String yamlString) {
-    echo "Hello, ${yamlString}"
+    echo "Called function"
     // Parse the YAML file and convert to JSON
     def database_name = sh(script: "echo ${yamlString} | yq e \'.database\'", returnStdout: true).trim()
+    echo "extracted database name"
     // Parse the YAML file and convert to JSON
     def schedule = sh(script: "echo ${yamlString} | yq e \'.schdeule\'", returnStdout: true).trim()
     echo "database name ${database_name}, ${schedule}"
