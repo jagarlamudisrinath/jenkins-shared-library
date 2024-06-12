ef62b34c3ffba89f059a717ed985acf03af0bae2
srinathjagarlamudi <jagarlamudisrinath99@gmail.com>
2024-06-11 19:50:05 -0500

    testing method

--- vars/extractSchedule.groovy
+++ vars/extractSchedule.groovy
@@ -1,8 +1,15 @@
-def call(String yamlString) {
-    echo "Hello, ${yamlString}"
-    // Parse the YAML file and convert to JSON
-    def database_name = sh(script: "echo ${yamlString} | yq e \'.database\'", returnStdout: true).trim()
-    // Parse the YAML file and convert to JSON
-    def schedule = sh(script: "echo ${yamlString} | yq e \'.schdeule\'", returnStdout: true).trim()
-    echo "database name ${database_name}, ${schedule}"
+// def call(String yamlString) {
+//     echo "Hello, ${yamlString}"
+//     // Parse the YAML file and convert to JSON
+//     def database_name = sh(script: "echo ${yamlString} | yq e \'.database\'", returnStdout: true).trim()
+//     // Parse the YAML file and convert to JSON
+//     def schedule = sh(script: "echo ${yamlString} | yq e \'.schdeule\'", returnStdout: true).trim()
+//     echo "database name ${database_name}, ${schedule}"
+// }
+
+// vars/extractSchedule.groovy
+def call(String yamlContent) {
+    // Your Groovy logic to process the YAML content
+    echo "Processing YAML: ${yamlContent}"
+    // Add your schedule extraction logic here
 }
