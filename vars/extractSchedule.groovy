c7086d38e23105a948fdd8d3cd36eac300d9b3fa
srinathjagarlamudi <jagarlamudisrinath99@gmail.com>
2024-06-11 20:02:41 -0500

    fixing error

--- vars/extractSchedule.groovy
+++ vars/extractSchedule.groovy
@@ -1,16 +1,12 @@
 def call(String yamlString) {
     echo "Called function"
-    // Parse the YAML file and convert to JSON
-    def database_name = sh(script: "echo ${yamlString} | yq e \'.database\'", returnStdout: true).trim()
-    echo "extracted database name"
-    // Parse the YAML file and convert to JSON
-    def schedule = sh(script: "echo ${yamlString} | yq e \'.schdeule\'", returnStdout: true).trim()
-    echo "database name ${database_name}, ${schedule}"
-}
 
-// // vars/extractSchedule.groovy
-// def call(String yamlContent) {
-//     // Your Groovy logic to process the YAML content
-//     echo "Processing YAML: ${yamlContent}"
-//     // Add your schedule extraction logic here
-// }
+    // Parse the YAML string and extract the database name and schedule
+    def database_name = sh(script: "echo \"${yamlString}\" | yq e '.database' -", returnStdout: true).trim()
+    echo "Extracted database name"
+
+    def schedule = sh(script: "echo \"${yamlString}\" | yq e '.schdeule' -", returnStdout: true).trim()
+    echo "Extracted schedule"
+
+    echo "Database name: ${database_name}, Schedule: ${schedule}"
+}
