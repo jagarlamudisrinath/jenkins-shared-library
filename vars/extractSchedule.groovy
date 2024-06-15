dc7193b5c604ff9284d572201d44b02cfe56033b
srinathjagarlamudi <jagarlamudisrinath99@gmail.com>
2024-06-14 22:21:53 -0500

    extract filenames

--- vars/extractSchedule.groovy
+++ vars/extractSchedule.groovy
@@ -1,13 +1,25 @@
-def call(String yamlString) {
-    echo "Called function"
-
-    // Parse the YAML string and extract the database name and schedule
-    def database_name = sh(script: "echo \"${yamlString}\" | yq e '.database' -", returnStdout: true).trim()
-    echo "Extracted database name"
-
+def call(String yamlFilePath) {
+    def database_name = getFileNameWithoutExtension(yamlFilePath)
+    def yaml = sh(script: "cat ${yamlFilePath}", returnStdout: true).trim()
     def schedule = sh(script: "echo \"${yamlString}\" | yq e '.schdeule' -", returnStdout: true).trim()
-    echo "Extracted schedule"
+
 
     echo "Database name: ${database_name}, Schedule: ${schedule}"
     return [database_name: database_name, schedule: schedule]
 }
+
+
+def getFileNameWithoutExtension(String filePath) {
+    if (filePath == null || filePath.isEmpty()) {
+        return filePath
+    }
+    def fileName = new File(filePath).name
+    if (fileName.endsWith(".yml")) {
+        return fileName[0..-5] // Remove the last 4 characters (".yml")
+    } else if (fileName.endsWith(".yaml")) {
+        return fileName[0..-6] // Remove the last 5 characters (".yaml")
+    } else {
+        return fileName // Return the original file name if it doesn't end with ".yml" or ".yaml"
+    }
+}
+
